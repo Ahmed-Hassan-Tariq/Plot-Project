5687db352abf30b349c206e94d71f9feb6eca2f4
Ahmed Hassan <atariq344@gmail.com>
2022-09-24 20:46:57 +0500

    Person class handle

--- src/Person.java
+++ src/Person.java
@@ -3,4 +3,18 @@ public class Person {
     private int age;
     private String date_of_birth;
     private String contact;
+
+    public Person(String name, int age, String date_of_birth, String contact) {
+        this.name = name;
+        this.age = age;
+        this.date_of_birth = date_of_birth;
+        this.contact = contact;
+    }
+    public Person() {}
+
+    public void print(){
+        System.out.printf("OWNER OF LAND%nName:%s%nAge:%d%nDOB:%s%nContact:%s", name, age, date_of_birth, contact);
+    }
+
+
 }
--- src/Plot_Test.java
+++ src/Plot_Test.java
@@ -1,7 +1,16 @@
 public class Plot_Test {
     public static void main(String[] args) {
 
+      Person owner = new Person("Ahmed", 12, "12/04/2002",  null);
+//        Person owner = new Person();
+        Plot newPlot = new Plot();
 
+        newPlot.setOwner(owner);
+
+
+        newPlot.getOwner().print();
     }
+
+
 
 }
